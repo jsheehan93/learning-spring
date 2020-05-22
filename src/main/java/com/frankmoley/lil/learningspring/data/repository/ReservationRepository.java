28ab9f3e0b0bf89fa40447d04e7f89917cd06454
jsheehan93 <jsheehan13@hotmail.co.uk>
2020-05-22 14:29:40 +0100

    First commit. Create service tier. Chapter 3 learning spring boot complete.

---
+++ src/main/java/com/frankmoley/lil/learningspring/data/repository/ReservationRepository.java
@@ -0,0 +1,14 @@
+package com.frankmoley.lil.learningspring.data.repository;
+
+import com.frankmoley.lil.learningspring.business.service.ReservationService;
+import com.frankmoley.lil.learningspring.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+
+    Iterable<Reservation> findReservationByReservationDate(Date date);
+}
